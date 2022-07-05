f91736682344da17a2cea177391a0788c5bcb62f
Evgeny Ovodkov <kefir4ek@gmail.com>
2022-07-06 00:42:47 +0300

    Fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
